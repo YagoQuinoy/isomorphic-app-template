4915ea3d618db0b737dc1788b85917f1899110cd
Yago Quiñoy Lobariñas <yquinoy@mega-media.es>
2017-05-23 15:20:23 +0200

    postcss, cssnext y custom extensions.

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,24 @@
-module.exports = {
+module.exports = (ctx) => ({
   plugins: {
-    'postcss-cssnext': {}
+    'postcss-import': {},
+    'postcss-url': {},
+    'postcss-cssnext': {
+      'features': {
+        'customMedia': {
+          'extensions': {
+            '--phone': '(min-width: 544px)',
+            '--tablet': '(min-width: 768px)',
+            '--desktop': '(min-width: 992px)',
+            '--large-desktop': '(min-width: 1200px)'
+          }
+        }
+      }
+    },
+    // add your "plugins" here
+    // ...
+    // and if you want to compress
+    // "cssnano",
+    'postcss-browser-reporter': ctx.env === 'development' ? {} : null,
+    'postcss-reporter': ctx.env === 'development' ? {} : null
   }
-}
+})
--- src/server/serverSideRendering/index.js
+++ src/server/serverSideRendering/index.js
@@ -24,11 +24,11 @@ const serverConfig = (config.env === 'development') ? config.webpackServer : con
 
 const favicon = `${serverConfig.url}:${serverConfig.port}/assets/favicon.ico`
 const scripts = [`${serverConfig.url}:${serverConfig.port}/assets/app.bundle.js`]
-const styles = `${serverConfig.url}:${serverConfig.port}/assets/styles.css`
-// let styles = ''
-// if (config.env === 'production') {
-//   styles = `${serverConfig.url}:${serverConfig.port}/assets/styles.css`
-// }
+
+let styles = ''
+if (config.env === 'production') {
+  styles = `${serverConfig.url}:${serverConfig.port}/assets/styles.css`
+}
 
 /**
  * Server rendering a React application
